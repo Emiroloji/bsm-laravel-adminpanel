d787818bc21fe547f6c3eb1cff14e6896742108c
Emircan Uysal <emircanuysal6e@gmail.com>
2025-04-27 16:43:11 +0300

    refactor sidebar layout to enhance CRM and ERP sections with new menu items

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -45,36 +45,60 @@
                     </div>
                 </div>
             </div>
+
+
+
+
+            <div class="menu-item pt-5">
+                <div class="menu-content">
+                    <span class="menu-heading fw-bold text-uppercase fs-7">Crm</span>
+                </div>
+            </div>
+            <div data-kt-menu-trigger="click" class="menu-item menu-accordion">
+                <span class="menu-link">
+                    <span class="menu-icon">
+                        <i class="ki-duotone ki-address-book fs-2">
+                            <span class="path1"></span><span class="path2"></span><span class="path3"></span>
+                        </i>
+                    </span>
+                    <span class="menu-title">Contact</span>
+                    <span class="menu-arrow"></span>
+                </span>
+                <div class="menu-sub menu-sub-accordion">
+                    <div class="menu-item">
+                        <a class="menu-link" href="{{ url('/crm/contacts') }}">
+                            <span class="menu-bullet"><span class="bullet bullet-dot"></span></span>
+                            <span class="menu-title fw-bold text-uppercase fs-7">Contact</span>
+                        </a>
+                    </div>
+                </div>
+                <div class="menu-sub menu-sub-accordion">
+                    <div class="menu-item">
+                        <a class="menu-link" href="{{ url('/crm/companies') }}">
+                            <span class="menu-bullet"><span class="bullet bullet-dot"></span></span>
+                            <span class="menu-title fw-bold text-uppercase fs-7">Companyes</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="menu-item pt-5">
+                <div class="menu-content">
+                    <span class="menu-heading fw-bold text-uppercase fs-7">Erp</span>
+                </div>
+            </div>
+            <div class="menu-item pt-5">
+                <div class="menu-content">
+                    <span class="menu-heading fw-bold text-uppercase fs-7">Stok Takip</span>
+                </div>
+            </div>
+            <div class="menu-item pt-5">
+                <div class="menu-content">
+                    <span class="menu-heading fw-bold text-uppercase fs-7">Vet sistem</span>
+                </div>
+            </div>
+
         </nav>
     </div>
 </aside>
-
-{{-- If you wish to use offcanvas on mobile, keep this section and add a toggle button somewhere in your layout: --}}
-{{--
-<button class="btn btn-icon d-lg-none mb-3" type="button"
-        data-bs-toggle="offcanvas"
-        data-bs-target="#kt_aside_offcanvas"
-        aria-controls="kt_aside_offcanvas">
-    <i class="ki-duotone ki-element-11 fs-1"></i>
-</button>
-
-<div class="offcanvas offcanvas-start d-lg-none" tabindex="-1" id="kt_aside_offcanvas"
-     aria-labelledby="kt_aside_offcanvas_label">
-    <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="kt_aside_offcanvas_label">Menu</h5>
-        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-    </div>
-    <div class="offcanvas-body px-0">
-        <div class="list-group list-group-flush">
-            <a href="{{ url('/') }}" class="list-group-item list-group-item-action d-flex align-items-center">
-                <i class="ki-duotone ki-element-11 fs-2 me-2"></i>
-                <span>Anasayfa</span>
-            </a>
-            <a href="{{ url('/todo') }}" class="list-group-item list-group-item-action d-flex align-items-center">
-                <i class="ki-duotone ki-element-11 fs-2 me-2"></i>
-                <span>Todo Page</span>
-            </a>
-        </div>
-    </div>
-</div>
---}}
